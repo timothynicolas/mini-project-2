aa675c16e741fb450f7628bfe7cd8ad4e511e61c
timothynicolas <timothynicolas24@gmail.com>
2023-10-14 21:21:28 +0800

    Modal pop-up & JS functions

---
+++ javascript/contact.js
@@ -0,0 +1,9 @@
+function submitConcern(){
+    let email = document.getElementById('email');
+    let name = document.getElementById('name');
+    let concern = document.getElementById('concern');
+
+    email.value = '';
+    name.value = '';
+    concern.value = '';
+}
