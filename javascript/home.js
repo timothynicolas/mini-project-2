69e75e1f037ed41bcb6958f04444f6fb50287ebc
timothynicolas <timothynicolas24@gmail.com>
2023-10-17 22:19:00 +0800

    edited about page

--- javascript/home.js
+++ javascript/home.js
@@ -1,12 +1,12 @@
-const url = 'http://127.0.0.1:5500/api/home_api.js';
+const url = 'http://127.0.0.1:5501/api/home_api.js';
 
 fetch(url)
 .then(response => response.json())
 .then(result => {
-    console.log(result)
+    
     
     let randomNumber = Math.floor(Math.random() * result.length);
-    console.log(randomNumber);
+    
 
    let randomQuote = result[randomNumber].quote;
 
