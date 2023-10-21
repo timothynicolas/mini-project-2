0155839418b6dc4d6a7acc9aaf3cc9ef2b0a5aa6
timothynicolas <timothynicolas24@gmail.com>
2023-10-21 10:08:44 +0800

    Updated API URL's

--- javascript/home.js
+++ javascript/home.js
@@ -1,4 +1,4 @@
-const url = 'http://127.0.0.1:5501/api/home_api.js';
+const url = 'https://timothynicolas.github.io/mini-project-2/api/home_api.js';
 
 fetch(url)
 .then(response => response.json())
