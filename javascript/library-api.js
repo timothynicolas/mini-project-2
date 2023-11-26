fef4c82d22aa94921bef4084f17901f9c971159d
timothynicolas <142022974+timothynicolas@users.noreply.github.com>
2023-11-26 18:31:17 +0800

    Update library-api.js
    para sa capstone

--- javascript/library-api.js
+++ javascript/library-api.js
@@ -3,7 +3,7 @@
     "id": "1",
     "bookName": "Atomic Habits",
     "price": "400",
-    "image": "../media/books/atomichabits.jpg",
+    "image": "media/atomichabits.jpg",
     "bookdescription": "Learn how tiny changes can lead to remarkable results in building good habits."
 },
 
@@ -11,7 +11,7 @@
     "id": "2",
     "bookName": "48 Laws of Power",
     "price": "450",
-    "image": "../media/books/48lawsofpower.jpg",
+    "image": "media/48lawsofpower.jpg",
     "bookdescription": "Explore timeless strategies for gaining power and influence in various situations."
     
 },
@@ -20,7 +20,7 @@
     "id": "3",
     "bookName": "How to Win Friends & Influence People",
     "price": "500",
-    "image": "../media/books/howtowinfriends.jpg",
+    "image": "media/howtowinfriends.jpg",
     "bookdescription": "Master the art of building relationships and influencing people positively."
     
 },
@@ -29,7 +29,7 @@
     "id": "4",
     "bookName": "Rich Dad, Poor Dad",
     "price": "400",
-    "image": "../media/books/richdadpoordad.jpg",
+    "image": "media/richdadpoordad.jpg",
     "bookdescription": "Discover valuable financial lessons from two contrasting perspectives."
     
 },
@@ -38,7 +38,7 @@
     "id": "5",
     "bookName": "A Mind for Numbers",
     "price": "450",
-    "image": "../media/books/amindfornumbers.jpg",
+    "image": "media/amindfornumbers.jpg",
     "bookdescription": "Unlock the secrets of effective learning and problem-solving in mathematics."
 },
 
@@ -46,7 +46,7 @@
     "id": "6",
     "bookName": "The Art of War",
     "price": "500",
-    "image": "../media/books/artofwar.jpg",
+    "image": "media/artofwar.jpg",
     "bookdescription": "Explore ancient strategies for successful conflict resolution and leadership."
     
 },
@@ -55,7 +55,7 @@
     "id": "7",
     "bookName": "The Subtle Art of Not Giving  a F*ck",
     "price": "400",
-    "image": "../media/books/subtleart.jpg",
+    "image": "media/subtleart.jpg",
     "bookdescription": "Discover a counterintuitive approach to living a good life."
    
 },
@@ -64,7 +64,7 @@
     "id": "8",
     "bookName": "To Kill a Mockingbird",
     "price": "450",
-    "image": "../media/books/tokillamockingbird.webp",
+    "image": "media/tokillamockingbird.webp",
     "bookdescription": "Witness the moral complexities of racial injustice in a southern town."
    
 },
@@ -73,7 +73,7 @@
     "id": "9",
     "bookName": "A Tale of Two Cities",
     "price": "500",
-    "image": "../media/books/ataleoftwocities.jpg",
+    "image": "media/ataleoftwocities.jpg",
     "bookdescription": "Experience the turmoil of two cities during the French Revolution."
    
 },
@@ -82,7 +82,7 @@
     "id": "10",
     "bookName": "The Lord of the Rings",
     "price": "400",
-    "image": "../media/books/lordoftherings.jpg",
+    "image": "media/lordoftherings.jpg",
     "bookdescription": "Embark on an epic journey through Middle-earth to destroy a powerful ring."
     
 },
@@ -91,7 +91,7 @@
     "id": "11",
     "bookName": "A Brief History of Time",
     "price": "450",
-    "image": "../media/books/briefhistoryoftime.jpg",
+    "image": "media/briefhistoryoftime.jpg",
     "bookdescription": "Explore the nature of the universe and concepts of time by Stephen Hawking."
 },
 
@@ -99,7 +99,7 @@
     "id": "12",
     "bookName": "The Elements of Style",
     "price": "500",
-    "image": "../media/books/elements of style.jpg",
+    "image": "media/elements of style.jpg",
     "bookdescription": "Master the art of clear and concise writing with this classic guide."
 },
 
@@ -107,7 +107,7 @@
     "id": "13",
     "bookName": "Principles of Neural Science",
     "price": "400",
-    "image": "../media/books/principlesofneural.jpg",
+    "image": "media/principlesofneural.jpg",
     "bookdescription": "Dive into the complex world of neuroscience and the human nervous system."
    
 },
@@ -116,7 +116,7 @@
     "id": "14",
     "bookName": "Principles of Marketing",
     "price": "450",
-    "image": "../media/books/principlesofmarketing.jpg",
+    "image": "media/principlesofmarketing.jpg",
     "bookdescription": "Gain insights into the fundamental principles of marketing strategies."
 },
 
@@ -124,7 +124,7 @@
     "id": "15",
     "bookName": "The Little Prince",
     "price": "500",
-    "image": "../media/books/thelittleprince.jpg",
+    "image": "media/thelittleprince.jpg",
     "bookdescription": "Embark on a magical journey with a young prince from a distant asteroid."
 },
 
@@ -132,7 +132,7 @@
     "id": "16",
     "bookName": "Sapiens",
     "price": "400",
-    "image": "../media/books/sapiens.jpg",
+    "image": "media/sapiens.jpg",
     "bookdescription": "Uncover the history and impact of Homo sapiens on the world."
 },
 
@@ -140,7 +140,7 @@
     "id": "17",
     "bookName": "Thinking Fast and Slow",
     "price": "450",
-    "image": "../media/books/thinkingfastandslow.jpg",
+    "image": "media/thinkingfastandslow.jpg",
     "bookdescription": "Explore the two systems of thought that drive our decisions by Daniel Kahneman."   
 },
 
@@ -148,7 +148,7 @@
     "id": "18",
     "bookName": "The Selfish Gene",
     "price": "500",
-    "image": "../media/books/selfishgene.jpg"
+    "image": "media/selfishgene.jpg"
    
 },
 
@@ -156,7 +156,7 @@
     "id": "19",
     "bookName": "The Structure of Scientific Revolutions",
     "price": "400",
-    "image": "../media/books/scientificrevolution.jpg",
+    "image": "media/scientificrevolution.jpg",
     "bookdescription": "Examine the paradigm shifts in science and the development of scientific theories by Thomas Kuhn."
 },
 
@@ -164,7 +164,7 @@
     "id": "20",
     "bookName": "Meditations",
     "price": "450",
-    "image": "../media/books/meditations.jpg",
+    "image": "media/meditations.jpg",
     "bookdescription": "Reflect on the philosophical thoughts of Roman Emperor Marcus Aurelius."
 },
 
@@ -172,7 +172,7 @@
     "id": "21",
     "bookName": "The Art of Computer Programming",
     "price": "500",
-    "image": "../media/books/computerprog.jpg",
+    "image": "media/computerprog.jpg",
     "bookdescription": "Dive into the world of algorithms and computer programming with Donald Knuth."
 },
 
@@ -180,7 +180,7 @@
     "id": "22",
     "bookName": "Introduction to the Theory of Programming",
     "price": "400",
-    "image": "../media/books/theorycomputation.jpg",
+    "image": "media/theorycomputation.jpg",
     "bookdescription": "Gain a foundational understanding of programming theory and concepts."
 },
 
@@ -188,7 +188,7 @@
     "id": "23",
     "bookName": "A Short History of Nearly Everything",
     "price": "450",
-    "image": "../media/books/shorthistory.jpg",
+    "image": "media/shorthistory.jpg",
     "bookdescription": "Embark on a journey through the history of science and our universe with Bill Bryson."
 },
 
@@ -196,7 +196,7 @@
     "id": "24",
     "bookName": "The Wright Brothers",
     "price": "500",
-    "image": "../media/books/wrightbrothers.jpg",
+    "image": "media/wrightbrothers.jpg",
     "bookdescription": "Learn about the pioneering journey of aviation by David McCullough."
 },
 
@@ -204,7 +204,7 @@
     "id": "25",
     "bookName": "A History of the World in 6 Glasses",
     "price": "400",
-    "image": "../media/books/6glasses.jpg",
+    "image": "media/6glasses.jpg",
     "bookdescription": "Discover the role of six iconic beverages in shaping human history."
 }
 ]
